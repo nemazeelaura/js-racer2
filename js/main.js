fd4ef3a437df011c5914cdce390d4e95333e3119
nemazeelaura <arvadajohnson@gmail.com>
2017-12-12 23:41:53 -0700

    final racer game

--- js/main.js
+++ js/main.js
@@ -1,37 +1,57 @@
 
 $(document).ready(function() {
-	console.log('hello friends'); 
+    console.log('hello friends');
 
 
   //redirect to game.html
-	$( 'button' ).click(function() {
-  		document.location.href = "game.html";
-	});
+    $( 'button' ).click(function() {
+          document.location.href = "game.html";
+    });
+        
+      	//setting variable for # of clicks on red and blue
+		var redCount = 0;
+		var blueCount = 0;
 
-  //click boxes to move
-  $( '.players > div' ).click(function() {
-  	console.log('hello');
-  	console.log($(this));
-  		// '.box'.keydown(function() {
-  		// 	console.log('hello');	
-	
-	});
-
-
+      	//click boxes to move
+      $( '.players > div' ).click(function() {
+          console.log('hello');
+          console.log($(this));
 
 
-});
+          $(this).animate({
+              left: '+=300',
+          }, 50 ,function() {
 
-		// 	$('.box').toggleClass('alt'); 
-	// 	$('.box').toggleClass('animated');
-	// });
+          	// getting the background color of the box clicked
+            var clr = $(this).css("background-color");
+            
+            if(clr === "rgb(0, 0, 255)" ) {
+                // if more than five clicks blue wins
+                blueCount++;
+
+                if(blueCount > 5) { 
+					alert("BLUE WINS!");
+                	// refresh page
+               		 location.reload(true);
+                }
+
+            } else if(clr === "rgb(255, 0, 0)" ) {
+                  // if more than five clicks red wins
+                  redCount++;
+				 if(redCount > 5) {
+				 	alert("RED WINS!");
+				 	// refresh page
+				 	location.reload(true);
+                }
+                	
+                
+            }
 
 
-	// $('#js-translate').click(function(e){
-		//   e.preventDefault();
-		//   $('.box').toggleClass('--move');
-		//   $('.box1').toggleClass('--move');
-		// });	
-		  		// '.box'.keydown(function() {
-  		// 	console.log('hello');	
+        });
+
+
+    });
+
+ });
 	
